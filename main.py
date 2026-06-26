a398d6a9868832b14338497f39ab729363221e85
Darwin4050E <dldiaz@espol.edu.ec>
2026-06-26 16:32:48 -0500

    feat: add semantic processing to main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pathlib import Path
 def run_analyzer():
 
     algorithms = [
-        #("tests/algorithm_darwin.php", "DarwinDiaz"),
-        ("tests/algorithm_gabriel.php", "GabrielTumbaco"),
+        ("tests/algorithm_darwin.php", "DarwinDiaz"),
+        #("tests/algorithm_gabriel.php", "GabrielTumbaco"),
     ]
 
     parser = PhpParser()
@@ -19,20 +19,30 @@ def run_analyzer():
             print(f"Error: File not found {path_algorithm}")
             continue
 
-        logger = PhpLogger(member_name=member, analysis_type="sintactico")
-
         with open(path_algorithm, "r", encoding="utf-8") as file:
             text = file.read()
 
         parser.lexer.input(text)
 
-        detected_errors = parser.parse(text)
+        sintactic_errors, semantic_errors = parser.parse(text)
+        
+        sintactic_logger = PhpLogger(member_name=member, analysis_type="sintactico")
 
-        if not detected_errors:
-            detected_errors = ["Análisis sintáctico exitoso. Código sin errores de estructura."]
+        if not sintactic_errors:
+            sintactic_errors = ["Análisis sintáctico exitoso. Código sin errores de estructura."]
 
-        logger.save_logs(detected_errors)
-        print(f"Log generado con éxito: {logger.file_path}")
+        sintactic_logger.save_logs(sintactic_errors)
+        print(f"Log sintáctico generado con éxito: {sintactic_logger.file_path}")
+
+        if "Análisis sintáctico exitoso" not in sintactic_errors[0]:
+            print(f"Fase semántica omitida para {member} debido a errores sintácticos previos.")
+
+        semantic_logger = PhpLogger(member_name=member, analysis_type="semantico")
+        
+        if not semantic_errors:
+            semantic_errors = ["Análisis semántico exitoso. Variables y contextos correctos."]
+        semantic_logger.save_logs(semantic_errors)
+        print(f"Log semántico generado con éxito: {semantic_logger.file_path}")
 
 if __name__ == "__main__":
     run_analyzer()
